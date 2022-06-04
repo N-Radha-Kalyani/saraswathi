04b573df535e0a8b0f07bf125879d4a9c93ca308
radha <radhala@gmail.com>
2022-06-04 16:19:33 +0530

    commit

--- swf/saraswathi/models.py
+++ swf/saraswathi/models.py
@@ -1,5 +1,11 @@
 from django.db import models
 
 # Create your models here.
-class subjects(models:Model):
+class subjects(models.Model):
+
+class users(models.Model):
+    name=CharField(max_length=30,label=Username)
+    email=EmailField(max_length=30,label=Email)
     
+
+
